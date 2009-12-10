045a42a862b27ced0fd4d62f7df7dead84d3ba4b
Amit Rathore <amitrathore@gmail.com>
2009-12-09 23:38:59 -0800

    added example using animal and cat

---
+++ spec/org/rathore/amit/frumios/trial.clj
@@ -0,0 +1,40 @@
+(ns frumios-spec)
+
+(use 'org.rathore.amit.frumios.core)
+
+(defclass animal 
+  (method sound []
+    "grr")
+  (method say-something []
+    (str (this :sound) ", I say!"))
+  (method move []
+    "going!"))
+
+(defclass cat
+  (:extends animal)
+  (method sound []
+    "meow!"))
+
+(def a (animal :new))
+(def c (cat :new))
+
+
+;frumios-spec> (a :sound)
+;"grr"
+;frumios-spec> (c :sound)
+;"meow!"
+;frumios-spec> (c :move)
+;"going!"
+;frumios-spec> (c :say-something)
+;";meow!, I say!"
+;frumios-spec> (a :say-something)
+;"grr, I say!"frumios-spec> (a :sound)
+;"grr"
+;frumios-spec> (c :sound)
+;"meow!"
+;frumios-spec> (c :move)
+;"going!"
+;frumios-spec> (a :say-something)
+;"grr, I say!"
+;frumios-spec> (c :say-something)
+;"meow!, I say!"
